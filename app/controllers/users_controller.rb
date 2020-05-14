ed10c3ac2a45881a5bec00cae9df9929144d9adc
sanix-darker <s4nixd@gmail.com>
2020-05-14 19:43:40 +0100

    Cleaning stuffs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.permit(:email, :password)
-  end
+    def user_params
+      params.permit(:email, :password)
+    end
 end
